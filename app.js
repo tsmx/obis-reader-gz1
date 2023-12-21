1f8449187a9d6be67a1019b42b23a682c500c65d
tsmx <dev@tsmx.net>
2023-12-21 22:01:02 +0100

    Switched to time-based polling of metering values

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 import { SerialPort } from 'serialport';
 import { DelimiterParser } from '@serialport/parser-delimiter';
+import { CronJob } from 'cron';
 import logger from './utils/logging.js';
 
 const regExBalance = /7-0:3\.0\.0\((\d*\.\d*)\*m3\)/g;
@@ -15,20 +16,26 @@ logger.info('SerialPort for ttyUSB1 created.');
 const parser = port.pipe(new DelimiterParser({ delimiter: '!' }));
 
 parser.on('data', function (data) {
-    logger.info(`Raw data: ${data}`);
-    let ds = data.toString();
+    let ds = data.toString('ascii');
     let balance = parseFloat([...ds.matchAll(regExBalance)][0][1]);
     let consumption = parseFloat([...ds.matchAll(regExConsumption)][0][1]);
     logger.info(`Balance: ${balance.toFixed(2)} Consumption: ${consumption.toFixed(2)}`);
 });
 
+const job = new CronJob(
+    '*/10 * * * * *', // cronTime
+    function () {
+        logger.info('Sending read request to GZ1...');
+        port.write('/?!\r\n');
+    }, // onTick
+    null, // onComplete
+    true, // start
+);
+
 process.on('SIGINT', function () {
+    job.stop();
     logger.info('Exiting...');
     process.exit();
 });
 
 logger.info('Start reading data, press CTRL+C to exit.');
-
-while (true) {
-    await new Promise(resolve => setTimeout(resolve, 1000));
-}
